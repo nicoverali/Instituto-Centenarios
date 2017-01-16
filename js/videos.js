a5a20b20630fd301cfe99eb9f5c4acbe7b431293
nicoverali <nicolasalevera98@gmail.com>
2017-01-16 01:05:11 -0300

    Solucione el problema de los botones de las categorias, y funcion a los triangulos

--- js/videos.js
+++ js/videos.js
@@ -1,10 +1,13 @@
 //GLOBAL VARIABLES
 var svgContainer
 var containers
+//Just for the triangles
+var iconContainers
 
 window.addEventListener("DOMContentLoaded", function(){
   svgContainer = document.getElementsByClassName("svg-container");
-  containers = document.getElementsByClassName("category-container")
+  containers = document.getElementsByClassName("category-container");
+  iconContainers = document.getElementsByClassName("category-icon-container");
   for (var i = 0; i < svgContainer.length; i++) {
     svgContainer[i].addEventListener("click", activateContainer, true)
   }
@@ -13,10 +16,12 @@ window.addEventListener("DOMContentLoaded", function(){
 function activateContainer(item){
   for (var i = 0; i < containers.length; i++) {
     if(svgContainer[i] == item.target){
-      containers[i].className = "category-container show"      
+      containers[i].className = "category-container show";
+      iconContainers[i].className = "category-icon-container show"
     }
     else {
-        containers[i].className = "category-container"
+      containers[i].className = "category-container";
+      iconContainers[i].className = "category-icon-container"
     }
   }
 }
