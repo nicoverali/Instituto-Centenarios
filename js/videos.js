e0ef44e23fe60132fedd82a766e68903639164a8
nicoverali <nicolasalevera98@gmail.com>
2017-01-19 18:32:19 -0300

    Finish videos list deploying

--- js/videos.js
+++ js/videos.js
@@ -8,8 +8,10 @@ var iconContainers
 var years
 var videosContainers
 var videosList
+var videoUnit
 var closeVideos
 var moreVideos
+var loadTime
 
 //GLOBAL VARIABLE FOR VIDEOPLAYER MODAL
 var playButtons
@@ -33,12 +35,17 @@ window.addEventListener("DOMContentLoaded", function(){
   years = document.getElementsByClassName("year");
   videosList = document.getElementsByClassName("videos-list")
   closeVideos = document.getElementsByClassName("close-videos")
+  moreVideos = document.getElementsByClassName("more-videos")
   videosContainers = document.getElementsByClassName("video-container")
+  videoUnit = document.getElementsByClassName("video-unit")
+  loadTime = new Array(videosList.length).fill(0)
 
   for(var i = 0; i < years.length; i++) {
     years[i].addEventListener("click", dropList);
-    if(closeVideos[i] != null)
+    if(closeVideos[i] != null){
     closeVideos[i].addEventListener("click", closeDropList);
+    moreVideos[i].addEventListener("click", loadMoreVideos)
+    }
   }
 
   //Setting variable and listener for videplayer modal
@@ -68,13 +75,30 @@ function activateContainer(item){
 }
 
 function dropList(item){
+  var actualVideoList
+  var firstFourVideosVar
   for (var i = 0; i < years.length; i++) {
     if (item.target == years[i]) {
+      actualVideoList = videosList[i]
+      firstFourVideosVar = 0
+      if(loadTime[i] == 0){
+        for (var x = 0; x < videoUnit.length; x++) {
+          if(videoUnit[x].parentNode == actualVideoList && firstFourVideosVar == 4){
+            videoUnit[x].className = "video-unit"
+          }
+          else if (videoUnit[x].parentNode == actualVideoList){
+            videoUnit[x].className = "video-unit show"
+            firstFourVideosVar++
+          }
+        }
+      }
+
       if (item.target.className == "year") {
         videosContainers[i].className = "video-container show"
       }
       else {
         videosContainers[i].className = "video-container"
+        loadTime[i] = 0
       }
       break;
     }
@@ -93,6 +117,43 @@ function closeDropList(item){
     if(item.target == closeVideos[i]){
       videosContainers[i].className = "video-container";
       years[i].className = "year";
+      loadTime[i] = 0;
+      break;
+    }
+  }
+}
+
+function loadMoreVideos(item){
+  var actualParent
+  var actualLoadTime
+  var iterationNum
+  for (var i = 0; i < moreVideos.length; i++) {
+    if(moreVideos[i] == item.target){
+      actualParent = videosList[i]
+      actualLoadTime = loadTime[i]
+      iterationNum = i
+      break;
+    }
+  }
+  var thisTime
+  thisTime = actualLoadTime
+  actualLoadTime = actualLoadTime + 4
+  for (var i = 0; i <= videoUnit.length; i++) {
+    console.log(i);
+    console.log(videoUnit.length);
+    console.log(thisTime);
+    console.log(actualLoadTime);
+    console.log(loadTime);
+    if(thisTime == actualLoadTime || i == videoUnit.length){
+      loadTime[iterationNum] = actualLoadTime;
+      break;
+    }
+    if((videoUnit[i].parentNode == actualParent) && (videoUnit[i].className != "video-unit show")){
+      videoUnit[i].className = "video-unit show";
+      thisTime = thisTime + 1;
+    }
+    else {
+      continue;
     }
   }
 }
