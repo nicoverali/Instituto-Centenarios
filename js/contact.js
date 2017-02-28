11aebd8177353014259c2020d8794d8112638cbc
nicoverali <nicolasalevera98@gmail.com>
2017-02-28 16:34:40 -0300

    Many changes, and contact page finish
    - Change show_home for not_show_home just on home
    - Change header(Maxi's way), with nav_active yaml variable
    -Change some title that had hr, for h1 with border-bottom
    - Added "Raleway 400"
    - Normalize titles margin top
    - Added "user-non-selectable" class to main-styles

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,77 @@
 var map;
-function initMap (){
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  })
+var myPos = {lat: 0, lng: 0, zoom:0, zoomControl: true};
+function initMap (newPos, newZoom){
+  if (newPos == undefined) {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: {lat: -38.8835, lng: -62.09},
+      zoom: 15,
+      disableDefaultUI: true,
+      zoomControl: true
+    })
+
+    var marker = new google.maps.Marker({
+      position: {lat: -38.885560, lng: -62.103000},
+      map: map,
+      title: 'Instituto Centenarios'
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: "Instituto Centenarios"
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+    //Set map with right properties for initial width of window
+    changeMap(window.innerWidth);
+  }
+  else {
+    if (newPos.zoom != map.getZoom()) {
+      map.setZoom(newPos.zoom)
+    }
+    if (newPos.lat != map.getCenter().lat() || newPos.lng != map.getCenter().lng()) {
+      map.setCenter({ lat: newPos.lat, lng: newPos.lng });
+    }
+    if (newPos.zoomControl != map.get('zoomControl')) {
+      map.setOptions({zoomControl: newPos.zoomControl});
+    }
+  }
+
+
+
 };
+
+window.addEventListener("resize", function(){
+  changeMap(window.innerWidth);
+})
+
+function changeMap(width){
+  if (width >= 993) {
+    myPos.lat = -38.8835;
+    myPos.lng = -62.09;
+    myPos.zoom = 15;
+    myPos.zoomControl = true
+    initMap(myPos);
+  }
+  else if(width <= 992 && width >= 670){
+    myPos.lat = -38.8835;
+    myPos.lng = -62.095;
+    myPos.zoom = 15;
+    myPos.zoomControl = true
+    initMap(myPos);
+  }
+  else if (width <= 669 && width >= 481) {
+    myPos.lat = -38.8835;
+    myPos.lng = -62.095;
+    myPos.zoom = 14;
+    myPos.zoomControl = true
+    initMap(myPos);
+  }
+  else if(width <= 480) {
+    myPos.lat = -38.8835;
+    myPos.lng = -62.093;
+    myPos.zoom = 14
+    myPos.zoomControl = false
+    initMap(myPos);
+  }
+}
